1ad0180c8837d0615fe7f4622bab0644a54ee81b
Arun Kumar Krishnan <emailarunkumar@gmail.com>
2018-02-01 23:37:43 +0530

    Update colocator.py

--- colocator.py
+++ colocator.py
@@ -1,4 +1,4 @@
-import pytz
+import pytz, sys
 from datetime import datetime
 from pytz import timezone
 
@@ -20,19 +20,35 @@ def check_conflicts(zone1, zone2, exclusive_duration = 9, overlap=0):
             return False
         else:
             return True
-        
 
-if __name__ == "__main__":
-#     print 'Timezone List'
-#     print pytz.all_timezones
-#     print pytz.common_timezones
-    zone2 = 'US/Pacific'
-    zone1 = 'Asia/Calcutta'
-    print check_conflicts(zone1, zone2, 9)
-    
+def generate_all_compatible_timezones():
     outFile = open('compatible_timezones.txt','w')
     for zone1 in pytz.common_timezones:
         for zone2 in pytz.common_timezones:
             if zone1 != zone2:
                 if check_conflicts(zone1, zone2, 9):
                     outFile.write(zone1 + ' : '+zone2+'\n')
+
+ 
+def get_compatible_timezones(from_timezone):
+    result = []
+    for zone in pytz.common_timezones:
+        if from_timezone != zone:
+            if check_conflicts(from_timezone, zone, 9):
+                result.append(zone)
+                
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) < 2:
+        print "USAGE:  python colocator.py  <<<TIME_ZONE>>>"
+        return
+    
+    from_timezone = sys.argv
+    
+    # zone2 = 'US/Pacific'
+    # zone1 = 'Asia/Calcutta'
+    # print check_conflicts(zone1, zone2, 9)
+    
+    print get_compatible_timezones(from_timezone)
+    
